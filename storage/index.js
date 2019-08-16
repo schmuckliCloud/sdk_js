f6c9846953350581526ed9ca3c2ba7808669ce7c
Julian Schmuckli <julian@schmuckli.net>
2019-08-16 22:30:36 +0200

    Improved first returns

--- storage/index.js
+++ storage/index.js
@@ -18,19 +18,39 @@ export class sCStorage {
   getAll(container_name){
     var global_this = this;
     return new Promise(function(resolve, reject) {
+      //Check the properties before sending to the API
+      if (container_name === undefined || container_name === "") {
+        reject(new Error("Please define a container."));
+      }
+
       axios.get(Config.API_ENDPOINT + "?bucket=" + global_this.bucket_id + "&dataset=" + global_this.dataset + "&container=" + container_name, {
         headers: {
           appid: global_this.appid,
           appsecret: global_this.appsecret
         }
       }).then(function(result){
-        resolve(result);
-      })
-
+        if(result.status === 200){
+          var result = new sCResult(result.data.status, result.data.message, result.data.body);
+          resolve(result);
+        } else {
+          reject(new Error("There was a problem with the API endpoint."));
+        }
+      });
     });
   }
+}
 
-  get api_endpoint() {
-    return Config.API_ENDPOINT;
+/*
+Result object for filtering the
+*/
+export class sCResult{
+  constructor(status_code, message, body){
+    this.status_code = status_code;
+    this.message = message;
+    this.data = body;
+  }
+
+  get isOK(){
+    return this.status_code >= 200 && this.status_code <= 299 ? true : false;
   }
 }
--- tests/test_client_app/src/App.vue
+++ tests/test_client_app/src/App.vue
@@ -1,30 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld :msg="test"/>
+    {{ message }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import { sCStorage } from 'schmucklicloud_storage';
-
-var ref = new sCStorage("4d4ff55414917628252ebcd373ade72e38005294", "17364a09ed982edb4089d5362e496b13e85b8060d474655ec6b58f57ea18aa60");
-ref.setDataset("abc");
-
-ref.setBucket(11);
-
-ref.getAll("posts");
 
 export default {
   name: 'app',
   data(){
     return {
-      test: ref.dataset
+      message: ""
     }
   },
-  components: {
-    HelloWorld
+  mounted(){
+    this.test_one();
+  },
+  methods: {
+    test_one(){
+      var global_this = this;
+
+      var ref = new sCStorage("4d4ff55414917628252ebcd373ade72e38005294", "17364a09ed982edb4089d5362e496b13e85b8060d474655ec6b58f57ea18aa60");
+      ref.setDataset("abc");
+
+      ref.setBucket(11);
+
+      ref.getAll("posts").then(function(result){
+        global_this.message = result;
+      }).catch(function(e) {
+        global_this.message = e;
+      });
+    }
   }
 }
 </script>
