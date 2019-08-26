8d3f59d6d4f83b1977166fbc0292b5a585cb82b6
Julian Schmuckli <julian@schmuckli.net>
2019-08-27 00:44:46 +0200

    Added examples

---
+++ examples/todo_list/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <form @submit="addTodo">
+      <input type="text" placeholder="Name" v-model="name">
+      <input type="submit" value="Add">
+    </form>
+
+    {{ todo_list }}
+  </div>
+</template>
+
+<script>
+import { sCStorage } from 'schmucklicloud_storage';
+
+var ref = new sCStorage('d8feb2cff54efd2bc9d6273541ec35aafb89f28f', 'b01b1edd64301f469c79e99950c0fe068996b1db2ca71672b73b7fb7ec9feccd');
+ref.setBucket(12);
+ref.setDataset("test3");
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      name: "",
+      todo_list: ""
+    }
+  },
+  mounted(){
+    var global_this = this;
+    ref.getAll("todos").then(function(response){
+      global_this.todo_list = response.data;
+    });
+  },
+  methods: {
+    addTodo(e){
+      e.preventDefault();
+      ref.insert("todos", {
+        name: this.name
+      });
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
