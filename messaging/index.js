980963458ab03c8d504e2d40e94a47bd64b41194
Julian Schmuckli <julian@schmuckli.net>
2020-04-24 16:31:50 +0200

    Added logic to the messaging functions

--- messaging/index.js
+++ messaging/index.js
@@ -2,65 +2,140 @@ import * as Config from "./config";
 import axios from "axios";
 
 class sCMessaging {
-    /**
-    Opens a new schmuckliCloud authentication instance. It can be used to manage then all the data in a defined project.
-    @param {String} app_id The APP ID, which was created for a client app in the schmuckliCloud console.
-    @param {String} app_secret The APP Secret, which was created for a client app in the schmuckliCloud console.
-    */
-    constructor(app_id, app_secret) {
-        this.appid = app_id;
-        this.appsecret = app_secret;
-    }
+  /**
+  Opens a new schmuckliCloud authentication instance. It can be used to manage then all the data in a defined project.
+  @param {String} app_id The APP ID, which was created for a client app in the schmuckliCloud console.
+  @param {String} app_secret The APP Secret, which was created for a client app in the schmuckliCloud console.
+  */
+  constructor(app_id, app_secret) {
+    this.appid = app_id;
+    this.appsecret = app_secret;
+  }
 
-    /**
-     * Assign a Firebase Cloud Messaging device token to a user in your project.
-     * @param {String} device_token The device token received from the Firebase SDK.
-     * @param {int} user_id The user id, received from the schmuckliCloud Auth SDK.
-     * @returns {Promise}
-     */
-    async assignTokenToUser(device_token, user_id) {
-        
-    }
+  /**
+   * Sets the currently signed in user.
+   * @param {string} auth_token Sets the auth token for the further operations.
+   */
+  setAuthToken(auth_token) {
+    this.auth_token = auth_token;
+  }
 
-    /**
-     * Sends a request to FCM instantly.
-     * @param {int} user_id The user id of the user which should be notified.
-     * @param {object} body The body of the request like here: https://firebase.google.com/docs/cloud-messaging/http-server-ref
-     * @return {Promise}
-     */
-    async sendRequestNow(user_id, body) {
-        
+  /**
+   * Assign a Firebase Cloud Messaging device token to the currently logged in your project.
+   * @param {String} device_token The device token received from the Firebase SDK.
+   * @returns {Promise}
+   */
+  async assignTokenToUser(device_token) {
+    if (this.auth_token === undefined) {
+      console.warn("Please define a auth_token with the function setAuthToken, before you can use this function.");
+      return false;
     }
+    return new Promise(async function (resolve, reject) {
+      var response = await axios({
+        url: Config.API_ENDPOINT,
+        method: "POST",
+        headers: {
+          appid: this.appid,
+          appsecret: this.appsecret,
+          authtoken: this.auth_token
+        },
+        data: {
+          function: "assign_token",
+          token: device_token
+        }
+      });
+      if (response.status_code === 200) {
+        resolve(response.body)
+      } else {
+        reject(response.body);
+      }
+    }.bind(this));
+  }
 
-    /**
-     * Sends a request to FCM on the specified timestamp.
-     * @param {int} user_id The user id of the user which should be notified.
-     * @param {object} body The body of the request like here: https://firebase.google.com/docs/cloud-messaging/http-server-ref
-     * @param {int} timestamp The timestamp, when the request should be processed.
-     * @return {Promise}
-     */
-    async sendRequestLater(user_id, body, timestamp) {
-        
+  /**
+   * Sends a request to FCM instantly with the currently logged in user.
+   * @param {object} body The body of the request like here: https://firebase.google.com/docs/cloud-messaging/http-server-ref
+   * @return {Promise}
+   */
+  async sendRequestNow(body) {
+    if (this.auth_token === undefined) {
+      console.warn("Please define a auth_token with the function setAuthToken, before you can use this function.");
+      return false;
     }
-}
+    return new Promise(async function (resolve, reject) {
+      var response = await axios({
+        url: Config.API_ENDPOINT,
+        method: "PUT",
+        headers: {
+          appid: this.appid,
+          appsecret: this.appsecret,
+          authtoken: this.auth_token
+        },
+        data: {
+          function: "call_service_now",
+          body: body
+        }
+      });
+      if (response.status_code === 200) {
+        resolve(response.body)
+      } else {
+        reject(response.body);
+      }
+    }.bind(this));
+  }
+
+  /**
+   * Sends a request to FCM on the specified timestamp with the currently logged in user..
+   * @param {object} body The body of the request like here: https://firebase.google.com/docs/cloud-messaging/http-server-ref
+   * @param {int} timestamp The timestamp, when the request should be processed.
+   * @return {Promise}
+   */
+  async sendRequestLater(body, timestamp) {
+      if (this.auth_token === undefined) {
+        console.warn("Please define a auth_token with the function setAuthToken, before you can use this function.");
+        return false;
+      }
+      return new Promise(async function (resolve, reject) {
+        var response = await axios({
+          url: Config.API_ENDPOINT,
+          method: "PUT",
+          headers: {
+            appid: this.appid,
+            appsecret: this.appsecret,
+            authtoken: this.auth_token
+          },
+          data: {
+            function: "call_service_now",
+            body: body,
+            timestamp: timestamp
+          }
+        });
+        if (response.status_code === 200) {
+          resolve(response.body)
+        } else {
+          reject(response.body);
+        }
+      }.bind(this));
+    }
+  }
 
 /*
 Result object for filtering the
 */
 class sCResult {
-    constructor(status_code, message, body) {
-      this.status_code = status_code;
-      this.message = message;
-      this.data = body;
-    }
-  
-    /**
-     * Returns true, if the request was successfully.
-     * @returns {boolean}
-     */
-    get isOK() {
-      return this.status_code >= 200 && this.status_code <= 299 ? true : false;
-    }
+  constructor(status_code, message, body) {
+    this.status_code = status_code;
+    this.message = message;
+    this.data = body;
   }
-  
-  export { sCMessaging, sCResult };  
+
+  /**
+   * Returns true, if the request was successfully.
+   * @returns {boolean}
+   */
+  get isOK() {
+    return this.status_code >= 200 && this.status_code <= 299 ? true : false;
+  }
+}
+
+export { sCMessaging, sCResult };  
